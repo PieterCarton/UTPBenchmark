73570590fdaa4d15892bae765a7462debeb44daa
Pieter Carton <pieterc.n@hotmail.com>
2024-03-01 11:04:03 +0100

    fix datarate always being shown as 0kbs

--- benchmark.bash
+++ benchmark.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # setup netem env here
-tc qdisc add dev lo root netem delay 250ms 10ms loss 1%
+tc qdisc add dev lo root netem delay 250ms 10ms loss 1\%
 java -jar configtestread.jar &
-java -jar configtestwrite.jar testPlan/testplan2.csv testData/testfile.jpg
+java -jar configtestwrite.jar "testPlan/testplan2.csv" "testData/testfile.jpg" 
