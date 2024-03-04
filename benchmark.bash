d166c3a22d400f66f3967cc99d02f3daf641603a
Pieter Carton <pieterc.n@hotmail.com>
2024-03-04 23:13:36 +0100

    add different test plan configurations

--- benchmark.bash
+++ benchmark.bash
@@ -2,4 +2,4 @@
 # setup netem env here
 tc qdisc add dev lo root netem delay 250ms 10ms loss 1\%
 java -jar configtestread.jar &
-java -jar configtestwrite.jar "testPlan/testplan2.csv" "testData/testfile.jpg" localhost 
+java -jar configtestwrite.jar "testPlan/congestion_window_increase_benchmark.csv" "testData/testfile.jpg" localhost 
